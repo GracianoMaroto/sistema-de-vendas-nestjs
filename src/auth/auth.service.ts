2e00a21c847f9fad2d61f17ff1dbace45138153c
Graciano Maroto <gracianoml07@gmail.com>
2025-10-24 12:04:21 -0300

    backend almost completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,37 +3,43 @@ import { Injectable } from '@nestjs/common';
 import { LoginDto } from './login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private prismaService: PrismaService,
+  ) {}
 
-    constructor(
-        private jwtService: JwtService, 
-        private prismaService: PrismaService
-    ) {}
+  async login(loginDto: LoginDto) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+            email: loginDto.email
+        },
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
 
-    async login(loginDto: LoginDto){
-        const user = await this.prismaService.user.findUnique({
-            where: { email: loginDto.email },
-        });
-        if(!user){
-            throw new Error('User not found');
-        }
+      const isPasswordValid = bcrypt.compareSync(
+        loginDto.password,
+        user.password,
+      );
+      if (!isPasswordValid) {
+        throw new Error('Invalid credentials');
+      }
 
-        const isPasswordValid = bcrypt.compareSync(
-            loginDto.password,
-            user.password,
-        );
-        if (!isPasswordValid){
-            throw new Error('Invalid credentials');
-        }
-
-        const token = this.jwtService.sign({ 
-            name: user.name, 
-            email: user.email, 
-            role: user.role, 
-            sub: user.id });
-        return { acess_token: token };
+      const token = this.jwtService.sign({
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        sub: user.id,
+      });
+      return { access_token: token };
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
