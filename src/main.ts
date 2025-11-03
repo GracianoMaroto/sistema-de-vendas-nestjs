6728a7ad0efcab4db373ebd29da23a242ce5bd74
Graciano Maroto <gracianoml07@gmail.com>
2025-11-03 11:46:57 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://sistema-aa.netlify.app', // seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // se você precisar enviar cookies
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server running on port ${port}`);
